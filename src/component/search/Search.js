9e4ac040a18ca7db2ee325552f7cab574f0d5782
Lukasz <lukasz.kopyto@gmail.com>
2020-04-10 19:36:28 +0200

    changing the Search component to functional with useState hook

--- src/component/search/Search.js
+++ src/component/search/Search.js
@@ -1,61 +1,51 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import SearchInput from './SearchInput'
 import SearchButton from './SearchButton'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-  state = {
-    text: '',
-  }
+const Search = ({ searchUser, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState('')
 
-  static propTypes = {
-    searchUser: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  }
+  const handleOnChange = (e) => setText(e.target.value)
 
-  handleOnChange = (e) => {
-    const { name, value } = e.target
-    this.setState({
-      [name]: value,
-    })
-  }
-
-  handleOnSubmit = (e) => {
+  const handleOnSubmit = (e) => {
     e.preventDefault()
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light')
+    if (text === '') {
+      setAlert('Please enter something', 'light')
     } else {
-      this.props.searchUser(this.state.text)
-      this.setState({ text: '' })
+      searchUser(this.state.text)
+      setText('')
     }
   }
 
-  render() {
-    const { showClear, clearUsers } = this.props
-    return (
-      <>
-        <form onSubmit={this.handleOnSubmit}>
-          <SearchInput
-            type='text'
-            name='text'
-            placeholder='Search Users...'
-            value={this.state.text}
-            onChange={this.handleOnChange}
-          />
-          <SearchButton type='submit' value='submit' width='80vw'>
-            Submit
-          </SearchButton>
-        </form>
-        {showClear && (
-          <SearchButton second={true} width='80vw' onClick={clearUsers}>
-            Clear
-          </SearchButton>
-        )}
-      </>
-    )
-  }
+  return (
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <SearchInput
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={handleOnChange}
+        />
+        <SearchButton type='submit' value='submit' width='80vw'>
+          Submit
+        </SearchButton>
+      </form>
+      {showClear && (
+        <SearchButton second={true} width='80vw' onClick={clearUsers}>
+          Clear
+        </SearchButton>
+      )}
+    </>
+  )
+}
+
+Search.propTypes = {
+  searchUser: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
